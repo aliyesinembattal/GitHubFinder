b829c8c909f99b39fb42f81859305a93013e5aaf
aliyesinembattal <aliyeesnm@gmail.com>
2021-02-08 06:35:54 +0300

    GitHub Finder Completed!

---
+++ app.js
@@ -0,0 +1,30 @@
+//Init Github
+const github = new GitHub();
+// Init UI
+const ui = new UI();
+
+// Search input
+const searchUser = document.getElementById("searchUser");
+
+// Search input event listener
+searchUser.addEventListener("keyup", (e) => {
+  // Get input text
+  const userText = e.target.value;
+
+  if (userText !== "") {
+    // Make http call
+    github.getUser(userText).then((data) => {
+      if (data.profile.message === "Not Found") {
+        // Show alert
+        ui.showAlert("USER NOT FOUND !", "alert alert-light");
+      } else {
+        // Show profile
+        ui.showProfile(data.profile);
+        ui.showRepos(data.repos);
+      }
+    });
+  } else {
+    // Clear profile
+    ui.clearProfile();
+  }
+});
---
+++ github.js
@@ -0,0 +1,26 @@
+class GitHub {
+  constructor() {
+    this.client_id = "ee34b8da6465e058c661";
+    this.client_secret = "9723501f3b5240e70c54a34e5e86ce8e28652114";
+    this.repos_count = 5;
+    this.repos_sort = "created: asc";
+  }
+
+  async getUser(user) {
+    const profileResponse = await fetch(
+      `https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`
+    );
+
+    const repoResponse = await fetch(
+      `https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`
+    );
+
+    const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
+
+    return {
+      profile,
+      repos,
+    };
+  }
+}
